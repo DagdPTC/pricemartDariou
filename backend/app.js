1445689533eb86b0999cf3d1e799895b39fb9ed2
Dario <20240167@ricaldone.edu.sv>
2026-04-16 15:44:47 -0600

    login and logout

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,10 @@ import reviewsRoutes from "./src/routes/reviewsRoutes.js"
 import brandsRoutes from "./src/routes/brands.js"
 import adminsRoutes from "./src/routes/admins.js"
 import clientsRoutes from "./src/routes/clients.js"
+
+import loginCustomerRoutes from "./src/routes/loginCustomer.js"
+import logoutRoutes from "./src/routes/logout.js"
+
 import registerClientsRoutes from "./src/routes/registerClients.js"
 import registerEmployeeRoutes from "./src/routes/registerEmployees.js"
 import cookieParser from "cookie-parser"
@@ -25,9 +29,13 @@ app.use("/api/employees", employeeRoutes);
 app.use("/api/reviews", reviewsRoutes);
 app.use("/api/brands", brandsRoutes);
 app.use("/api/admins", adminsRoutes);
-app.use("/api/clients", clientsRoutes)
-app.use("/api/registerClients", registerClientsRoutes)
-app.use("/api/registerEmployee", registerEmployeeRoutes)
+app.use("/api/clients", clientsRoutes);
+app.use("/api/registerClients", registerClientsRoutes);
+app.use("/api/registerEmployee", registerEmployeeRoutes);
+app.use("/api/loginClients", loginCustomerRoutes);
+app.use("/api/logout", logoutRoutes);
+
+
 
 
 export default app;
